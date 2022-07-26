2505ed5a73f8494e6e191edbc12bc18cde31e982
zieeco <zieecosam@gmail.com>
2022-07-26 23:43:16 +0100

    Add relatioship to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :recipes
+  has_many :foods
+
   validates :name, presence: true
 end
