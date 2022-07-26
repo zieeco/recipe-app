5a6c50d9ea216959531be81ce6a32f74b675f39e
samiullah <bahadursamiullah@gmail.com>
2022-07-26 14:00:57 +0430

    Add relation recipe, food and recipe_food

--- db/migrate/20220726092000_create_recipe_foods.rb
+++ db/migrate/20220726092000_create_recipe_foods.rb
@@ -2,7 +2,8 @@ class CreateRecipeFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
       t.integer :quantity
-
+      t.belongs :recipe, index:true , foreign_key:true
+      t.belongs :food, index:true , foreign_key:true
       t.timestamps
     end
   end
