b37ba8e325e1991da949e57d09c3f0b62f0580ba
zieeco <zieecosam@gmail.com>
2022-07-27 21:42:35 +0100

    Fix error when deleting a logged in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,47 +5,38 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  # GET /users/new
+  def show
+    @user = User.find_by(id: current_user.id)
+  end
+
   def new
     @user = User.new
   end
 
-  def show
-    @user = User.find_by(id: params[:id])
-  end
-
-  # edit GET /users/id/edit
   def edit; end
 
-  # POST /users or /users.json
   def create
     @user = User.new(user_params)
 
     respond_to do |format|
       if @user.save
         format.html { redirect_to user_url(@user), notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /users/1 or /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to user_url(@user), notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /users/1 or /users/1.json
   def destroy
     @user.destroy
 
@@ -57,7 +48,6 @@ class UsersController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find_by(id: params[:id])
   end
