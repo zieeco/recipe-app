f5693580d8fa22871d99dffedd8856a031583ad0
zieeco <zieecosam@gmail.com>
2022-07-29 19:31:07 +0100

    Add method for total price and total food

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,4 +6,16 @@ class Recipe < ApplicationRecord
   validates :preparation_time, presence: true
   validates :cooking_time, presence: true
   validates :description, presence: true
+
+  def total_foods
+    recipe_foods.size
+  end
+
+  def total_price
+    total = 0
+    recipe_foods.each do |recipe|
+      total += recipe.food.price * recipe.food.quantity
+    end
+    total
+  end
 end
